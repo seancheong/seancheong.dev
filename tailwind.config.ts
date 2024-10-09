76895099ced44128f9a92a5613d4fae92bd07492
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2024-10-09 21:50:58 +0900

    chore: Setup tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        border: 'var(--secondary)',
+        input: {
+          background: 'var(--background)',
+          foreground: 'var(--foreground)'
+        },
+        icon: {
+          background: 'var(--icon-background)',
+          foreground: 'var(--foreground)'
+        },
+        chip: {
+          background: 'var(--card-background)',
+          foreground: 'var(--chip-foreground)'
+        },
+        timeline: 'var(--timeline)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
