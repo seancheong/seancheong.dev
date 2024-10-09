4e8f6d0ab5835996857b1581213f517e8b8d6f2b
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2024-10-09 22:10:29 +0900

    chore: Setup eslint and prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,31 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
+
 import './globals.css';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
   variable: '--font-geist-sans',
-  weight: '100 900'
+  weight: '100 900',
 });
 const geistMono = localFont({
   src: './fonts/GeistMonoVF.woff',
   variable: '--font-geist-mono',
-  weight: '100 900'
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
   title: 'Sean Cheong Zhen Xiong',
-  description: 'A software engineer'
+  description: 'A software engineer',
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
