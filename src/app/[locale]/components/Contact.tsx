72023169cbc9c6a810d3c1077b28b5ec57084529
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2025-02-23 19:48:45 +0900

    chore: UI improvements
    - Add tooltips for Contact section icons
    - Update Project Script Blender url
    - Rephrase Profile paragraph

--- src/app/[locale]/components/Contact.tsx
+++ src/app/[locale]/components/Contact.tsx
@@ -1,8 +1,15 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons';
 import { SquareLibraryIcon } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import Link from 'next/link';
+import type { PropsWithChildren } from 'react';
 
 import { SendEmailButton } from './SendEmailButton';
 
@@ -34,34 +41,65 @@ export const Contact = () => {
         />
 
         <div className="flex gap-6">
-          <Link
-            href="https://www.linkedin.com/in/zhen-xiong-cheong-b80630ba/"
-            target="_blank"
-            className="hover:text-sky-600"
-          >
-            <LinkedInLogoIcon className="h-6 w-6" />
-            <span className="sr-only">{t('Contact.aria-labels.linkedin')}</span>
-          </Link>
+          <TooltipProvider>
+            <LinkTooltip tooltipText={t('Contact.tooltips.linkedin')}>
+              <Link
+                href="https://www.linkedin.com/in/zhen-xiong-cheong-b80630ba/"
+                target="_blank"
+                className="hover:text-sky-600"
+              >
+                <LinkedInLogoIcon className="h-6 w-6" />
+                <span className="sr-only">
+                  {t('Contact.aria-labels.linkedin')}
+                </span>
+              </Link>
+            </LinkTooltip>
 
-          <Link
-            href="https://github.com/seancheong"
-            target="_blank"
-            className="hover:text-gray-900"
-          >
-            <GitHubLogoIcon className="h-6 w-6" />
-            <span className="sr-only">{t('Contact.aria-labels.github')}</span>
-          </Link>
+            <LinkTooltip tooltipText={t('Contact.tooltips.github')}>
+              <Link
+                href="https://github.com/seancheong"
+                target="_blank"
+                className="hover:text-gray-900"
+              >
+                <GitHubLogoIcon className="h-6 w-6" />
+                <span className="sr-only">
+                  {t('Contact.aria-labels.github')}
+                </span>
+              </Link>
+            </LinkTooltip>
 
-          <Link
-            href="https://medium.com/@seancheongzhenxiong"
-            target="_blank"
-            className="hover:text-teal-600"
-          >
-            <SquareLibraryIcon />
-            <span className="sr-only">{t('Contact.aria-labels.medium')}</span>
-          </Link>
+            <LinkTooltip tooltipText={t('Contact.tooltips.medium')}>
+              <Link
+                href="https://medium.com/@seancheongzhenxiong"
+                target="_blank"
+                className="hover:text-teal-600"
+              >
+                <SquareLibraryIcon />
+                <span className="sr-only">
+                  {t('Contact.aria-labels.medium')}
+                </span>
+              </Link>
+            </LinkTooltip>
+          </TooltipProvider>
         </div>
       </div>
     </div>
   );
 };
+
+interface LinkTooltipProps {
+  tooltipText: string;
+}
+
+const LinkTooltip = ({
+  children,
+  tooltipText,
+}: PropsWithChildren<LinkTooltipProps>) => (
+  <Tooltip>
+    <TooltipTrigger asChild>{children}</TooltipTrigger>
+
+    <TooltipContent>
+      <p>{tooltipText}</p>
+    </TooltipContent>
+  </Tooltip>
+);
